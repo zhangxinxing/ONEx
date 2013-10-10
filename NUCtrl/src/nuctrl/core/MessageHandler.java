6bfc9ea0b1d8f4e999c8bc9f91972d7ed3808bc7
Colin Zhang <hello.zhang1992@gmail.com>
2013-10-10 12:02:04 +0800

    had better merge MessageHandler into one

---
+++ NUCtrl/src/nuctrl/core/MessageHandler.java
@@ -0,0 +1,14 @@
+package nuctrl.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fan
+ * Date: 13-10-10
+ * Time: AM11:53
+ */
+public class PacketHandler {
+
+    public void onPacket(){
+
+    }
+}
