8a5fff78bf6c0db9c6ff613d5e99cf93e8c80a28
Colin Zhang <hello.zhang1992@gmail.com>
2013-10-31 16:04:22 +0800

    Fix #30, add hazelcast.xml

---
+++ ONEx/src/ONExBox/test/THazelcast.java
@@ -0,0 +1,11 @@
+package ONExBox.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fan
+ * Date: 13-10-31
+ * Time: PM3:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class THazelcast {
+}
