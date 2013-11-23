577e528fa6ba0c569673e6ef04d68e1a2b414923
Colin Zhang <hello.zhang1992@gmail.com>
2013-11-24 02:00:43 +0800

    support dist in build

--- build2.sh
+++ build2.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/sh -ex
 ./build.sh -s 6700 -o 6633 -r 8080 -d 7888 -y
 ./build.sh -s 6800 -o 6634 -r 8081 -d 7889 -y
+if !(test -z $1)
+then
+rm -rf release-$1
+cp -rf release release-$1;
+fi
+
+
+
